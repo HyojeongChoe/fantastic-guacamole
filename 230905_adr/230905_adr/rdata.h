dbf1466d712646b254b680a4ee0dc0fe07e16749
chjeong <144426107+HyojeongChoe@users.noreply.github.com>
2023-09-13 17:55:58 +0900

    Add files via upload

---
+++ 230905_adr/230905_adr/rdata.h
@@ -0,0 +1,15 @@
+#ifndef __RDATA_H__
+#define __RDATA_H__
+
+#define RAND_SIZE	10		//소스코드에서 일일이 하드코딩 하지않고 이렇게 관리
+
+const int id[] = { 111, 222, 333, 444, 555 };
+const char* name[] = { "홍길동", "전우치", "강감찬", "유관순", "이순신" };
+const char* tel[] = { "010-1234-1234", "010-1234-1111", "010-1234-2222",
+	"010-1234-3333", "010-1234-4444" };
+const char* addr[] = { "대구 동구 신암4동", "서울 동구 신암4동" ,
+	"광주 동구 신암4동", 	"대전 동구 신암4동", "부산 동구 신암4동" };
+const char* email[] = { "hong@naver.com", "joen@kakao.com",
+	"kang@gmail.com", "yu@msn.com", "lee@daum.net" };
+
+#endif
